cfacabfd1e47a07e0a31e7de97bad6c681a4499d
karun <1harunkorkmaz@gmail.com>
2024-03-20 15:18:10 +0300

    add

--- WebApplication6/Program.cs
+++ WebApplication6/Program.cs
@@ -1,20 +1,38 @@
 var builder = WebApplication.CreateBuilder(args);
 
-builder.Services.AddEndpointsApiExplorer();
-builder.Services.AddSwaggerGen();
-
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (app.Environment.IsDevelopment())
-{
-    app.UseSwagger();
-    app.UseSwaggerUI();
-}
-
-app.UseHttpsRedirection();
+var myTodos = new List<Todo>();
 
 app.MapGet("/", () => "hello world");
 
+app.MapGet("/todos", () => myTodos);
+
+app.MapPost("/todos", (Todo todo) =>
+{
+    myTodos.Add(todo);
+    return Results.Created($"/todos/{todo.Id}", todo);
+});
+
+app.MapGet("/todos/{id}", (int id) => myTodos.SingleOrDefault(x => x.Id == id));
+
+app.MapDelete("/todos/{id:int}", (int id) =>
+{
+    var obj = myTodos.SingleOrDefault(x => x.Id == id);
+    if (obj == null)
+    {
+        return Results.NotFound("obje bulunamadi");
+    }
+    myTodos.Remove(obj);
+    return Results.Ok("obje silindi");
+});
+
 app.Run();
 
+
+public class Todo
+{
+    public int Id { get; set; }
+    public string Title { get; set; } = null!;
+    public DateTime Date { get; set; }
+}
