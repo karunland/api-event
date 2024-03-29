9f3024fd8ed8bf2cd8605209952cd27216012ed8
karun <1harunkorkmaz@gmail.com>
2024-03-29 11:52:18 +0300

    new update for all

--- WebApplication6/Model/Todo.cs
+++ WebApplication6/Model/Todo.cs
@@ -1,13 +1,11 @@
 ﻿using System.ComponentModel.DataAnnotations;
 
-namespace WebApplication6.Model
+namespace WebApplication6.Model;
+public class Todo
 {
-    public class Todo
-    {
-        public int Id { get; set; }
-        public string Title { get; set; } = null!;
-        public DateTime Date { get; set; }
-        [Required]
-        public string? MyDescription { get; internal set; }
-    }
+    public int Id { get; set; }
+    public string Title { get; set; } = null!;
+    public DateTime Date { get; set; }
+    [Required]
+    public string? MyDescription { get; internal set; }
 }
