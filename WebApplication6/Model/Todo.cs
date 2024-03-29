4354dfc482f4f2aff7c1e7711f754654fdac94f7
karun <1harunkorkmaz@gmail.com>
2024-03-29 11:56:54 +0300

    aa

--- WebApplication6/Model/Todo.cs
+++ WebApplication6/Model/Todo.cs
@@ -4,8 +4,7 @@ namespace WebApplication6.Model;
 public class Todo
 {
     public int Id { get; set; }
-    [Required]
-    public string? Title { get; set; }
+    public string Title { get; set; } = null!;
     public DateTime Date { get; set; }
     [Required]
     public string? MyDescription { get; internal set; }
