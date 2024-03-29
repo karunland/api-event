60ef678d7c6a92e003bfb11a06e47aa41ee59970
karun <1harunkorkmaz@gmail.com>
2024-03-29 11:56:22 +0300

    new update to both repos

--- WebApplication6/Model/Todo.cs
+++ WebApplication6/Model/Todo.cs
@@ -4,7 +4,8 @@ namespace WebApplication6.Model;
 public class Todo
 {
     public int Id { get; set; }
-    public string Title { get; set; } = null!;
+    [Required]
+    public string? Title { get; set; }
     public DateTime Date { get; set; }
     [Required]
     public string? MyDescription { get; internal set; }
